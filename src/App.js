c6552cbab05c46d42bb79db0a95993b0f649b996
Samir Vertahov <77025552+SamirVRTH@users.noreply.github.com>
2022-08-09 14:11:20 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import Message from "./Message";
+
+function App() {
+  const name = 'Тест3';
+  return (
+   <Message name={name} />
+  );
+}
+
+export default App;
---
+++ src/Message.js
@@ -0,0 +1,9 @@
+function Message(props) {
+    return (
+      <div>
+        Привет, {props.name}!
+      </div>
+    );
+  }
+  
+  export default Message;
